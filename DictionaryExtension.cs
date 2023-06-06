e5129946a46bbe8b74e03806b0d902f8b89cae64
Richard PAYET <rick.info.rsoc@gmail.com>
2023-06-06 23:10:27 +0200

    1er commit

---
+++ DictionaryExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Transverse._Common.Dictionary;
+
+public static class DictionaryExtension
+{
+    public static string ToKeyValueString<V>(this IDictionary<string, V> dictionary, string keyValueSeparator = ";", string quoteValueSymbol = "", string keyValueEqualitySymbol = "=") 
+    {
+        var retour = string.Empty;
+        
+        ICollection<string> strings = new Collection<string>();
+        string str;
+        foreach (var keyValue in dictionary)
+        {
+            str = $"{keyValue.Key}{keyValueEqualitySymbol}{quoteValueSymbol}{keyValue.Value}{quoteValueSymbol}";
+            strings.Add(str);
+        }
+
+        retour = string.Join(keyValueSeparator, strings);
+
+        return retour;
+    }
+}
